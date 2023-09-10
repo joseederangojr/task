6d2327187c098202941f2a24392e19fc74d27bfc
Jose Ederango Jr <joseederangojr@gmail.com>
2023-09-10 15:09:39 +0800

    add case converter middleware

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -40,8 +40,10 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\ConvertCase\ConvertRequestToSnakeCase::class,
+            \App\Http\Middleware\ConvertCase\ConvertResponseToCamelCase::class
         ],
     ];
 
