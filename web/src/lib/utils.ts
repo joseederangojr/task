237fd60afac67108ddf92dda9139d6b08dd474f2
Jose Ederango Jr <joseederangojr@gmail.com>
2023-09-17 10:27:47 +0800

    refactor: remove nextjs

--- web/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-/**
-  * Tailwind CSS classnames generator
-  */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
