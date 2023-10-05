4251daa2f07b2e52b4dc90648cd29588cb8db03a
Jose Ederango Jr <joseederangojr@gmail.com>
2023-10-05 17:45:25 +0800

    refactor: signup api and view

--- resources/js/pages/auth/signup/components/signup-form.tsx
+++ resources/js/pages/auth/signup/components/signup-form.tsx
@@ -1,111 +1,38 @@
-import * as z from 'zod'
-import { FieldPath, useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from "@tanstack/react-query";
+
 import { Form, FormField, FormItem, FormLabel, FormMessage, FormDescription, FormControl } from '@/components/ui/form'
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { useToast } from '@/components/ui/use-toast';
-import { axios } from '@/lib/axios'
-import { AppError, UnknownError, ValidationError } from "@/lib/errors";
-import { AppResponseData, AppResponseError } from '@/types';
-import { ToastAction } from '@/components/ui/toast';
-import { Link, router } from '@inertiajs/react';
+import { Link, useForm } from '@inertiajs/react';
 
 
 
-const schema = z.object({
-  name: z.string().nonempty(),
-  email: z.string().email(),
-  password: z.string().nonempty().min(8),
-  passwordConfirmation: z.string().nonempty(),
-}).refine(data => data.password === data.passwordConfirmation, {
-  message: 'Passwords do not match',
-  path: ['passwordConfirmation']
-})
 
-type FormData = z.infer<typeof schema>
-
-type AuthSignUpResponse = {
-  authorization: {
-    token: string,
-    type: 'bearer',
-  }
-}
-
-const signUp = async (data: FormData) => {
-  return await axios.post('/auth/signup', data)
-    .catch((err: Error) => {
-      console.error('signUp error', err)
-      if (err instanceof ValidationError) {
-        throw err
-      }
-
-      if (err instanceof AppError) {
-        throw new UnknownError(err.data.message);
-      }
-
-      throw new UnknownError(err.message);
-    })
-}
 
 function SignUpForm() {
-  const { toast } = useToast();
   const form = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      passwordConfirmation: ''
-    }
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
   })
-  const signUpMutatio = useMutation<
-    AppResponseData<AuthSignUpResponse>,
-    AppResponseError<FormData>,
-    FormData
-  >(signUp);
 
-  const onSubmit = async (data: FormData): Promise<void> => {
-    const response = await signUpMutatio.mutateAsync(data).catch((err: Error) => {
-      if (err instanceof ValidationError) {
-        for (const k in err.errors) {
-          const errorKey = k as FieldPath<FormData>
-          return form.setError(errorKey, {
-            type: 'manual',
-            message: err.errors[errorKey]?.[0]
-          })
-        }
-        return;
-      }
 
-      if (err instanceof UnknownError) {
-
-        toast({
-          title: "Uh oh! Something went wrong.",
-          description: "There was a problem with your request.",
-          action: <ToastAction altText="Try again" onClick={() => router.reload()}>Reload</ToastAction>,
-        })
-        return;
-      }
-    })
-
-    if (!response) return
-    window.localStorage.setItem('task.auth.token', response.data.authorization.token)
-    router.visit('/', { replace: true })
+  const onSubmit = async (event: React.BaseSyntheticEvent) => {
+    event.preventDefault()
+    form.post('/api/auth/signup')
   }
 
   return (
     <Form {...form}>
-      <form className='flex flex-col space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
+      <form className='flex flex-col space-y-4' onSubmit={onSubmit}>
         <FormField
-          control={form.control}
+          data={form.data}
           name="name"
-          render={({ field }) => (
+          render={({ field, setValue }) => (
             <FormItem>
-              <FormLabel htmlFor="name">Name</FormLabel>
+              <FormLabel htmlFor={field.id}>Name</FormLabel>
               <FormControl>
-                <Input id="name" {...field} />
+                <Input onChange={event => setValue(event.target.value)} {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -113,16 +40,16 @@ function SignUpForm() {
         />
 
         <FormField
-          control={form.control}
+          data={form.data}
           name="email"
-          render={({ field, fieldState }) => (
+          render={({ field, error, setValue }) => (
             <FormItem>
-              <FormLabel htmlFor="email">Email</FormLabel>
+              <FormLabel htmlFor={field.id}>Email</FormLabel>
               <FormControl>
-                <Input type="email" id="email" {...field} />
+                <Input type="email" onChange={event => setValue(event.target.value)} {...field} />
               </FormControl>
               {
-                fieldState?.error ? <FormMessage /> : (
+                error ? <FormMessage /> : (
                   <FormDescription>
                     We won&apos;t sell your email address
                   </FormDescription>
@@ -133,13 +60,13 @@ function SignUpForm() {
         />
 
         <FormField
-          control={form.control}
+          data={form.data}
           name="password"
-          render={({ field }) => (
+          render={({ field, setValue }) => (
             <FormItem>
               <FormLabel htmlFor="password">Password</FormLabel>
               <FormControl>
-                <Input type="password" id="password" {...field} />
+                <Input type="password" onChange={event => setValue(event.target.value)} {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -147,13 +74,13 @@ function SignUpForm() {
         />
 
         <FormField
-          control={form.control}
+          data={form.data}
           name="passwordConfirmation"
-          render={({ field }) => (
+          render={({ field, setValue }) => (
             <FormItem>
-              <FormLabel htmlFor="passwordConfirmation">Password Confirmation</FormLabel>
+              <FormLabel htmlFor={field.id}>Password Confirmation</FormLabel>
               <FormControl>
-                <Input type="password" id="passwordConfirmation" {...field} />
+                <Input type="password" onChange={event => setValue(event.target.value)} {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -162,8 +89,8 @@ function SignUpForm() {
 
 
         <FormItem>
-          <Button className='mt-2 w-full' type="submit" disabled={form.formState.isSubmitting}>
-            {form.formState.isSubmitting ? 'Signing up...' : 'Sign up'}
+          <Button className='mt-2 w-full' type="submit" disabled={form.processing}>
+            {form.processing ? 'Signing up...' : 'Sign up'}
           </Button>
         </FormItem>
         <div className='flex  justify-center text-sm'>
